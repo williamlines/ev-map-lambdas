ba81d1fb2f57c8d455ef567a6d56ea67823a974e
William Lines <williamlines42@gmail.com>
2023-02-03 10:37:54 +0000

    added directions function

--- lambdas/directions.js
+++ lambdas/directions.js
@@ -1,15 +1,13 @@
 "use strict";
 
+const MAPBOX_API_TOKEN = process.env.MAPBOX_API_TOKEN;
+
 module.exports.directions = async (event) => {
-  const MAPBOX_API_TOKEN = process.env.MAPBOX_API_TOKEN;
-  const route = await fetch(
-    `https://api.mapbox.com/directions/v5/mapbox/cycling/-84.518641,39.134270;-84.512023,39.102779?geometries=geojson&access_token=${MAPBOX_API_TOKEN}`
-  );
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        route: route,
+        message: data,
         input: event,
       },
       null,
