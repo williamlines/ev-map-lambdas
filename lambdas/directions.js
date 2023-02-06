b5be04d321825abfd221f86b57bf3256261b2511
William Lines <williamlines42@gmail.com>
2023-02-06 15:01:58 +0000

    can now retrieve and navigate between origin and destination sent in query string

--- lambdas/directions.js
+++ lambdas/directions.js
@@ -3,8 +3,11 @@
 const MAPBOX_API_TOKEN = process.env.MAPBOX_API_TOKEN;
 
 module.exports.directions = async (event) => {
-  console.log(event);
-  const res = await getDirections();
+  const origin = event.queryStringParameters.origin || "-84.518641,39.134270";
+  const destination =
+    event.queryStringParameters.destination || "-84.518641,39.134270";
+
+  const res = await getDirections(origin, destination);
   const data = await res.json();
   const response = {
     statusCode: 200,
@@ -12,13 +15,16 @@ module.exports.directions = async (event) => {
     body: JSON.stringify({
       message: data,
       input: event,
+      queryStringParameters: event.queryStringParameters,
     }),
   };
   return response;
 };
 
-async function getDirections() {
-  const url = `https://api.mapbox.com/directions/v5/mapbox/cycling/-84.518641,39.134270;-84.512023,39.102779?geometries=geojson&access_token=${MAPBOX_API_TOKEN}`;
+// `https://api.mapbox.com/directions/v5/mapbox/cycling/-84.518641,39.134270;-84.512023,39.102779?geometries=geojson&access_token=${MAPBOX_API_TOKEN}`
+
+async function getDirections(origin, destination) {
+  const url = `https://api.mapbox.com/directions/v5/mapbox/cycling/${origin};${destination}?geometries=geojson&access_token=${MAPBOX_API_TOKEN}`;
   let res;
   try {
     res = await fetch(url);
